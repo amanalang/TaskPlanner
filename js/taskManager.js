0f061ae49547ccf80750029ff45b7df864b9a1fe
jensenrex <jensenrex@gmail.com>
2021-09-22 15:02:57 +1000

    finish task 5

---
+++ js/taskManager.js
@@ -0,0 +1,11 @@
+class TaskManager {
+  constructor() {
+    this.tasks = [];
+  }
+  function addTask (newTask) {
+    this.tasks.push(); 
+  }
+}
+
+let shopping = new TaskManager;
+console.log(drive);
